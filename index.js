6b25d8f91fc8cff52c2be7d19c7ce48d4aa41f94
laizaarruda <laiza.arruda@hotmail.com>
2024-12-10 15:36:14 -0300

    Classificador js

---
+++ index.js
@@ -0,0 +1,30 @@
+/* Desafio Felipao (DIO) - Classificador de nível de Herói */
+
+let nomeHeroi = prompt("Nome do herói:");
+let xpHeroi = parseInt(prompt("Quantidade de experiência do herói:"));
+
+function classificadorHeroi(nomeHeroi, xpHeroi) {
+
+    let nivelHeroi = "";
+
+    if (xp < 1000) {
+        nivelHeroi = "Ferro";
+    } else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+        nivelHeroi = "Bronze";
+    } else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
+        nivelHeroi = "Prata";
+    } else if (xpHeroi >= 5001 && xpHeroi <= 7000) {
+        nivelHeroi = "Ouro";
+    } else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
+        nivelHeroi = "Platina";
+    } else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
+        nivelHeroi = "Ascendente";
+    } else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
+        nivelHeroi = "Imortal";
+    } else if (xpHeroi >= 10001) {
+        nivelHeroi = "Radiante";
+
+        console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivelHeroi}`);
+
+    }
+}
